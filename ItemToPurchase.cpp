d5887667b9343ef19d2e61a55829f2a902a0c994
halimurph <167351997+halimurph@users.noreply.github.com>
2024-04-19 10:29:30 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -4,3 +4,12 @@ using namespace std;
 #include "ItemToPurchase.h"
 
 /* Type your code here */
+ItemToPurchase::ItemToPurchase(){}
+
+void ItemToPurchase::SetName(){
+  string itemName = name;
+}
+
+void ItemToPurchase::GetName(){
+  cout << name << endl;
+}
