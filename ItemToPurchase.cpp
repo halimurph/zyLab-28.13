5e8afb87efe30e0699931e85000ad71f3ddd1091
halimurph <167351997+halimurph@users.noreply.github.com>
2024-04-19 13:24:45 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -31,10 +31,5 @@ int ItemToPurchase::GetQuantity(){
 }
 
 void ItemToPurchase::PrintInfo(){
-  cout << "TOTAL COST" << endl;
-   totalCost = (item1.GetQuantity() * item1.GetPrice()) + (item2.GetQuantity() * item2.GetPrice());
-   cout << item1.GetName() << item1.GetQuantity() << " @ $" << item1.GetPrice() << " = $" << (item1.GetQuantity() * item1.GetPrice()) << endl; 
-   cout << item2.GetName() << item2.GetQuantity() << " @ $" << item2.GetPrice() << " = $" << (item2.GetQuantity() * item2.GetPrice()) << endl; 
-   cout << "Total: $" << totalCost << endl;
-
+  cout << itemName << " " << itemQuantity << " @ $" << itemPrice << " = $" << (itemPrice * itemQuantity) << endl;
 }
